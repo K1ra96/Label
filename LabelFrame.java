5a542d242ba03d72c416cc144964efff8add2063
K1ra96 <136936389+K1ra96@users.noreply.github.com>
2024-02-21 15:52:42 -0300

    Create LabelFrame.java

---
+++ LabelFrame.java
@@ -0,0 +1,25 @@
+import java.awt.FlowLayout;
+import javax.swing.Jframe;
+import javax.swing.Jlabel;
+import javax.swing.Swingconstants;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class LabelFrame extends Jframe{
+    private Jlabel label1;
+    private Jlabel label2;
+    private Jlabel label3;
+    
+    public LabelFrame(){
+        super("testing Jlabel");
+        setLayout(new FlowLayout());
+        
+        label1 = new Jlabel ( "INSTITUTO FEDERAL DE EDUCAÇÃO, CIÊNCIA E TECNOLOGIA DE MATO GROSSO");
+        add (label1);
+        
+        icon ifmt = new ImageIcon( getClass().getResource("ifmt.png"));
+        label2 = new Jlabel ("ifmt", ifmt, SwingConstants.LEFT);
+        add(label2);
+        
+    }
+}
